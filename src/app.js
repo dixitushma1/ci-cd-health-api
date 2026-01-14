97535b0de4d9c7e94f13c3448576a3ab5b5b63c2
Ushma <ushma@42works.net>
2026-01-14 17:02:29 +0530

    minor test checks attempt 3

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'ok' });
+  res.status(200).json({ status: 'ok', message: 'Testing CI CD functionality completely' });
 });
 
 module.exports = app;
